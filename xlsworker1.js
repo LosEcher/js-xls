df423c17fdc49380ffc621f96067294da5d2ae43
SheetJS <dev@sheetjs.com>
2017-02-25 18:47:15 -0800

    updating to 0.7.6

--- xlsworker1.js
+++ xlsworker1.js
@@ -1,4 +1,4 @@
-/* xls.js (C) 2013-2015 SheetJS -- http://sheetjs.com */
+/* xls.js (C) 2013-present SheetJS -- http://sheetjs.com */
 /* uncomment the next line for encoding support */
 //importScripts('dist/cpexcel.js');
 importScripts('xls.js');
@@ -26,4 +26,3 @@ onmessage = function (oEvent) {
   var r = s2ab(res.d)[1];
   postMessage(r, [r]);
 };
-
